5a28cb48c64506b5a236b30ebe881264e1a1be55
bonup-dev <noreply@bonup.local>
2026-02-13 21:05:16 +0000

    Add gitignore and clean repo

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yw)3s4c3ipi^-323)_$)nnck=mbntc8ha9*rvu(#(#av__-)_#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contracts',
+    'contracts'
+    
 ]
 
 MIDDLEWARE = [
